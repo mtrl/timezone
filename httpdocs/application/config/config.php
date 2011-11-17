837192fd8741f61594a0b2087daefc9b568cd387
= <=>
2011-11-17 00:12:37 +0000

    Updated auto base_url code

--- httpdocs/application/config/config.php
+++ httpdocs/application/config/config.php
@@ -14,9 +14,8 @@
 | path to your installation.
 |
 */
-$root = "http://".$_SERVER['HTTP_HOST'];
-$root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
-$config['base_url']    = $root; 
+$config['base_url'] = "http://".$_SERVER['HTTP_HOST'];
+$config['base_url'] .= preg_replace('@/+$@','',dirname($_SERVER['SCRIPT_NAME'])).'/'; 
 
 /*
 |--------------------------------------------------------------------------
